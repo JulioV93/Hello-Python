caa427ac43787fd9816909c197c9db7006a99b0d
JulioM <julio.machadov93@gmail.com>
2024-04-07 18:28:01 -0400

    01_Configuracion_02_HelloWorld_03_Variables

---
+++ 00_helloworld.py
@@ -0,0 +1,28 @@
+# Esto es un comentario:
+# Hola mundo
+
+print("Hola python")
+print('Hola python')
+
+
+"""
+Este es un
+comentarios
+en varias lineas
+"""
+
+'''
+Este es un
+comentarios
+en varias lineas
+'''
+
+#Consulyando el tipo de dato
+
+print(type("Hola Python")) #Tipo string
+print(type(5)) #Tipo int
+print(type(1.5)) #Tipo float
+print(type(3 + 1j)) #Tipo complex number
+print(type(True)) #Tipo bool
+
+print(type(print("Ejemplo de NoneType"))) #Tipo 'NoneType'
