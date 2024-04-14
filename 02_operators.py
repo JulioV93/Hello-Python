9ec3ca25be0164986cb3cc918602cd4a33ee972b
JulioM <julio.machadov93@gmail.com>
2024-04-14 19:01:56 -0400

    02_clase_operadores

---
+++ 02_operators.py
@@ -0,0 +1,44 @@
+### Operadores Aritmeticos ###
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 3)
+print(10 // 3)
+print(2 ** 3)
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+print("Hola " + " Python " + " ¿Qué Tal?")
+print("Hola " + str(5))
+print("Hola " * 5)
+print("Hola " * (2 ** 3))
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+### Operadores Comparativos ###
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("aaaa" >= "abaa") #Ordenación Alfabética por ASCII
+print(len("aaaa") >= len("abaa")) # Ordenación Alfabética
+print("Hola" <= "Python")
+print("Hola" == "Hola")
+print("Hola" != "Python")
+
+### Operadores Lógicos ###
+
+print(3 > 4  and "Hola" > "Python")
+print(3 > 4  or "Hola" > "Python")
+print(3 < 4  and "Hola" < "Python")
+print(3 < 4  or "Hola" > "Python")
+print(3 < 4  or ("Hola" > "Python" and 4 == 4))
+print(not(3 > 4))
