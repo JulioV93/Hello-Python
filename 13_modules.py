5bd9a4da53807248ff2175ed1d3f64f0d6621636
JulioM <julio.machadov93@gmail.com>
2024-05-01 20:05:44 -0400

    Clase_Modulos

---
+++ 13_modules.py
@@ -0,0 +1,25 @@
+### Modules ###
+
+#El archivo 10_functions tira error por la nomenclatura del nombre dle fichero, por lo que se copio y creo uno nuevo.
+
+#Manera de llamar otros ficheros.
+import my_module
+
+my_module.sumValue(1,2,3)
+my_module.printValue("Hola Python")
+
+#manera de llamar una unica funcionalidad del modulo o fichero creado
+from my_module import sumValue, printValue
+
+sumValue(1,2,3)
+printValue("Hola Python")
+
+#Python cuenta con varios modulos preinstalados, los cuales se pueden llamar y utilizar
+import math
+
+print(math.pi)
+print(math.pow(2,8))
+
+from math import pi as PI_VALUE
+
+print(PI_VALUE)
---
+++ my_module.py
@@ -0,0 +1,7 @@
+
+
+def sumValue(number_one, number_two, number_three):
+    print(number_one + number_two + number_three)
+
+def printValue(text):
+    print(text)
