4844541e09570c72eb75c008f42a82ca55467a32
JulioM <julio.machadov93@gmail.com>
2024-05-11 22:06:48 -0400

    Clase_Parametros

--- Backend/FastAPI/users.py
+++ Backend/FastAPI/users.py
@@ -8,15 +8,16 @@ app = FastAPI()
 #BaseModel da la capacidad de crear una entidad.
 
 class User(BaseModel):
+    id_usuario: int
     name: str
     surname: str
     url: str
     age: int
     
 users_list = [
-        User(name="Julio",surname="Machado",url="https://github.com/JulioV93",age=30),
-        User(name="Alejandro",surname="Vasquez",url="https://github.com/JulioV93",age=34),
-        User(name="Vanessa",surname="Moncayo",url="https://github.com/JulioV93",age=28)
+        User(id_usuario=1,name="Julio",surname="Machado",url="https://github.com/JulioV93",age=30),
+        User(id_usuario=2,name="Alejandro",surname="Vasquez",url="https://github.com/JulioV93",age=34),
+        User(id_usuario=3,name="Vanessa",surname="Moncayo",url="https://github.com/JulioV93",age=28)
         ]
 
 @app.get("/usersjson")
@@ -28,8 +29,31 @@ async def usersjson():
 #Al usar BaseModel como constructor, se indica cual dato es cada uno para que no exista confusión interna, la clase User esta replicando el comportamiento de BaseModel
 @app.get("/usersclass")
 async def usersclass():
-    return User(name="Julio",surname="Machado",url="https://github.com/JulioV93",age=30)
+    return User(id_usuario=1,name="Julio",surname="Machado",url="https://github.com/JulioV93",age=30)
 
 @app.get("/users")
 async def users():
     return users_list
+
+#Manera de pasar parametros por GET
+
+#1 - PATH
+#Se usa el path o ruta de la url para pasar parametros y poder obtener los datos especificos.
+#http://127.0.0.1:8000/user/1
+@app.get("/user/{id_usuario}")
+async def user(id_usuario: int):
+        return search_users(id_usuario)
+
+#2 - Query
+#Pasar por query es pasar todos los parametros despues del ? y se concatena con & cada parametro
+#http://127.0.0.1:8000/userquery/?id_usuario=4&name=Julio
+@app.get("/user/")
+async def user(id_usuario: int):
+    return search_users(id_usuario)
+    
+def search_users(id_usuario: int):
+    users = filter(lambda user: user.id_usuario == id_usuario, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"error": "No se ha encontrado el usuario especificado."}
